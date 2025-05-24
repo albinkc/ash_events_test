2688b80949aa8e313a635350f2be1717bc19ee27
Albin Kocheril Chacko <8876252+albinkc@users.noreply.github.com>
2025-05-25 02:14:37 +0530

    add ash_admin

--- lib/ash_events_test/events.ex
+++ lib/ash_events_test/events.ex
@@ -1,6 +1,9 @@
 defmodule AshEventsTest.Events do
-  use Ash.Domain,
-    otp_app: :ash_events_test
+  use Ash.Domain, otp_app: :ash_events_test, extensions: [AshAdmin.Domain]
+
+  admin do
+    show? true
+  end
 
   resources do
     resource AshEventsTest.Events.Event
--- lib/ash_events_test/test_domain.ex
+++ lib/ash_events_test/test_domain.ex
@@ -1,6 +1,9 @@
 defmodule AshEventsTest.TestDomain do
-  use Ash.Domain,
-    otp_app: :ash_events_test
+  use Ash.Domain, otp_app: :ash_events_test, extensions: [AshAdmin.Domain]
+
+  admin do
+    show? true
+  end
 
   resources do
     resource AshEventsTest.TestDomain.TestResource
